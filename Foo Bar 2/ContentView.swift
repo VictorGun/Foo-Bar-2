f7b2a803cd84aa2e16961b5cb7752de8478f5eb3
VictorGun <Thunderboomboom7@gmail.com>
2022-08-29 09:48:34 -0500

    Initial Commit

---
+++ Foo Bar 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Foo Bar 2
+//
+//  Created by Victor Gunderson on 8/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Foo Bar 2/Foo_Bar_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Foo_Bar_2App.swift
+//  Foo Bar 2
+//
+//  Created by Victor Gunderson on 8/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Foo_Bar_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
